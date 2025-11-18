dcb17fb1f8ccbe336ee455726f33bba754ccebea
trang <huyentrang15625@gmail.com>
2025-11-18 07:22:39 +0700

    update bai 2

--- ss1/bai2.py
+++ ss1/bai2.py
@@ -1,11 +1,17 @@
 a = 1
 print(f"Giá trị của a là: {a}")
+print(f"kiểu dữ liệu của a là: {type(a)}")
 
 b = 2.5
 print(f"Giá trị của b là: {b}")
+print(f"kiểu dữ liệu của a là: {type(b)}")
+
 
 c = "Xin chào, Cheng!"
 print(f"Giá trị của c là: {c}")
+print(f"kiểu dữ liệu của a là: {type(c)}")
+
 
 d = True
 print(f"Giá trị của d là: {d}")
+print(f"kiểu dữ liệu của a là: {type(d)}")
